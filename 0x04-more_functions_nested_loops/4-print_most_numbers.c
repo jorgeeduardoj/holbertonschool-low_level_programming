80074d2ea93a64642833f45ae2d30d2655996727
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-24 21:05:41 +0000

    fix function 4-print_most_numbers: fix name of function

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * print_numbers -  prints the numbers, from 0 to 9
+ * print_most_numbers -  prints the numbers, from 0 to 9 without 2 and 4
  *
  * Return:  void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
