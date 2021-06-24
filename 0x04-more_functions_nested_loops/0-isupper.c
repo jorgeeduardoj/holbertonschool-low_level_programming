d53b4e87076b1f7856088464e0ae2733a925634e
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-24 20:33:07 +0000

    fix 2 isupper funcion: more betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
  * _isupper -  checks for uppercase character
- *
+ *@c: letter to be evaluated
  * Return:  1 if is upper || 0 if is not.
  */
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
