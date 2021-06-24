d13adc6af64521a8231ed47cc0baf530d3cb0847
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-24 22:40:53 +0000

    fix function 5-more_numbers: fix loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,24 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 1; j++)
 		{
-			_putchar(j / 10 + '0');
-			if (j > 9)
+			for (k = 0; k <= 9; k++)
 			{
-				_putchar(j % 10 + '0');
-				if (j == 14)
+				if (j == 1 && k <= 4)
 				{
-					_putchar('\n');
+					_putchar(j + '0');
+				}
+				if (j == 0 || (j == 1 && k <= 4))
+				{
+					_putchar(k + '0');
 				}
 			}
 		}
+		_putchar('\n');
 	}
 }
