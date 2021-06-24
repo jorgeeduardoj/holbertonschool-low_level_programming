ffb4b2a2567e137fb32dc8ea164ec6fa193ae513
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-24 11:59:09 +0000

    fix function 2-largest_number try 2

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+	a= -12;
+	b= 40;
+	c= 7;
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+	a= -20;
+	b= -3;
+	c= 0;
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+	return (0);
+}
