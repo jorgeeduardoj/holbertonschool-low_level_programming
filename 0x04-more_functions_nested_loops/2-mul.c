e92ebf01b91e1cf8435c54919d5dd45098265017
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-24 20:43:48 +0000

    add function 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul -  multiplies two integers
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return:  1 if is upper || 0 if is not.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
