58c99d91e77f6bec5077f4e016354fdbd9970060
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-24 21:24:35 +0000

    fix function 5-more_numbers: accomplish condition just 3 _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
+			_putchar(j / 10 + '0');
+			if (j > 9)
 			{
-				_putchar(j);
-			}
-			else
-			{
-				_putchar(j / 10 + '0');
 				_putchar(j % 10 + '0');
 				if (j == 14)
 				{
