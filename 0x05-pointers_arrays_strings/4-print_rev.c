bbe0bdf869e408024d6a3e1cfd4a7becab8b77af
jorgeeduardoj <jorgeeduardo@inspyra.co>
2021-06-28 17:05:19 +0000

    add function 9-strcpy

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string
+ * Return: length of a string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
