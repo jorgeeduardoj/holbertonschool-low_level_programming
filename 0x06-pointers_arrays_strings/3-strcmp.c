72ffb81e489b87fd7dde28becce78146c4b9c957
jorgeeduardo <jorgeeduardo@inspyra.co>
2021-07-02 20:45:32 -0500

    add function 3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: compare
+ * @s2: compare
+ * Return: char*
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
